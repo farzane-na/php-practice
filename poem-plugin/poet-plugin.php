9c44d4d0b94fa9807046ae1e968a8ca33063b9a9
farzane nazmabadi <1379farzanenazmabadi@gmail.com>
2024-10-14 08:09:04 +0330

    create poem plugin

---
+++ poem-plugin/poet-plugin.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Plugin Name:       افزونه شعر گویی
+ * Plugin URI:        https://farzanenazmabadi.liara.run
+ * Description:       این افزونه هر موقع که وارد داشبورد میشوید یک شعر زیبا برای شما نمایش می دهد.😍💜
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            فرزانه نظم آبادی
+ * Author URI:        https://farzanenazmabadi.liara.run
+ * Text Domain:       poet-plugin
+ * Domain Path:       /languages
+ */
+
+ global $poems;
+ $poems=array (
+    "آن شب خوش را به سادگی نپذیر!",
+    " پیری را نابود و در انتهای روز طغیان کن!",
+    "طغیان کن... طغیان کن علیه مرگِ روشنایی ",
+    "اگر چه خردمندان در نهایت تاریکی را حقیقت می دانند",
+    "زیرا که کلامشان به نبود روشنایی  انجامیده",
+    " اما آنها آن شب خوش را به سادگی نپذیرفتند",
+    "طغیان کن علیه مرگ روشنایی ..."
+ );
+ function getRandomPoem(){
+   global $poems;
+    $randomIndex=random_int(0, count($poems)-1);
+    return  $poems[$randomIndex] ;
+ };
+ function writePoemAfterInit(){
+    return add_filter( "admin_footer_text", "getRandomPoem" );
+ };
+ add_action( "init", "writePoemAfterInit" );
